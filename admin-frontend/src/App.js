ac5e8b3dea0898033e522705a6ed3a86053baf02
jjjjonathan <hornathanjon@gmail.com>
2021-06-16 00:58:09 -0500

    work on nav drawer

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,19 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import LoginPage from './components/LoginPage';
 import NavDrawer from './components/NavDrawer';
 
+const baseUrl = 'http://localhost:5000';
+
 const App = () => {
-  const handleLogin = (values) => {
-    console.log(values);
+  const [blogs, setBlogs] = useState([]);
+  const [token, setToken] = useState('');
+  const [user, setUser] = useState();
+
+  // Effect hook for fetching current user object from stored token
+  useEffect(() => {
+    const storedToken = localStorage.getItem('odinBlogPublicToken');
+    const storedUserId = localStorage.getItem('odinBlogPublicUserId');
+
+    if (storedToken && storedUserId && !token && !user) {
+      fetch(`${baseUrl}/api/users/${storedUserId}`, {
+        headers: {
+          Authorization: `Bearer ${storedToken}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((fetchedUser) => {
+          setToken(storedToken);
+          setUser(fetchedUser);
+        })
+        .catch((error) => {
+          console.error(error);
+          if (error.message === 'Failed to fetch') {
+            // TODO ('Failed to load data from server');
+          } else {
+            // TODO (error.message);
+          }
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // function is async so that formik will automatically setSubmitting to false on completion
+  const handleLogin = async (values) => {
+    fetch(`${baseUrl}/api/users/login`, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((loginInfo) => {
+        if (loginInfo.token) {
+          const { _id, username, email, admin } = loginInfo;
+
+          setToken(loginInfo.token);
+
+          localStorage.setItem('odinBlogPublicToken', loginInfo.token);
+          localStorage.setItem('odinBlogPublicUserId', _id);
+
+          setUser({ _id, username, email, admin });
+
+          // TODO (`Successfully logged in as ${username}!`);
+        } else {
+          // TODO ('Invalid username or password');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        if (error.message === 'Failed to fetch') {
+          // TODO ('Failed to load data from server');
+        } else {
+          // TODO (error.message);
+        }
+      });
   };
 
-  return (
+  return user ? (
     <>
       <NavDrawer />
-      <LoginPage handleLogin={handleLogin} />
     </>
+  ) : (
+    <LoginPage handleLogin={handleLogin} />
   );
 };
 
--- admin-frontend/src/components/NavDrawer.js
+++ admin-frontend/src/components/NavDrawer.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  Drawer,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    padding: theme.spacing(3),
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
 
 const NavDrawer = () => {
+  const classes = useStyles();
+
   return (
-    <Drawer variant="permanent" anchor="left">
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Typography align="center" variant="h6" className={classes.title}>
+        odinblog admin
+      </Typography>
+      <Divider />
       <List>
         <ListItem button>
           <ListItemText>Posts</ListItemText>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button>
+          <ListItemText>Log Out</ListItemText>
         </ListItem>
       </List>
     </Drawer>
