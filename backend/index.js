fbc0e86b3a3e077fc19709bb4beb20bef7e5c792
jjjjonathan <hornathanjon@gmail.com>
2021-06-20 23:19:21 -0500

    prepare to deploy

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
